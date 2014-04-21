35974e6f595b691a93b10a418b6548198de55ca1
rektide <rektide@voodoowarez.com>
2014-04-20 22:38:30 -0400

    Add json2yaml.

--- yaml-writer.js
+++ yaml-writer.js
@@ -12,7 +12,6 @@ function YamlWriter(opts){
 	}
 	opts= opts||{}
 	opts.objectMode= true
-	//opts.encoding= "utf8"
 	stream.Transform.call(this, opts)
 
 	this.configure(opts)
@@ -24,14 +23,15 @@ YamlWriter.prototype._transform= function(chunk,enc,cb){
 		stanza= {}
 		for(var i= 0; i< this.keys.length; ++i){
 			var key= this.keys[i]
-			stanza[key]= chunk[key]
+			stanza[key]= chunk[i]
 			
 		}
 		this.template.render(stanza)
 	}else{
 		var stanza= _.clone(chunk)
 	}
-	stanza.comments= stanza.comments.join("\n")
+	if(stanza.comments)
+		stanza.comments= stanza.comments.join("\n")
 	delete stanza.quoteValue
 	stanza.stanza= stanza
 	var render= this.template.render(stanza)
@@ -42,7 +42,7 @@ YamlWriter.prototype.configure= function(opts){
 	this.nunjucks= new nunjucks.Environment(new nunjucks.FileSystemLoader(".", true), {autoescape: false})
 	var template= opts.template||"yaml.nun"
 	this.template= this.nunjucks.getTemplate(template, true)
-	this.keys= this.keys||["key","value"]
+	this.keys= opts.keys||["key","value"]
 
 	this._readableState.objectMode = false;
 	this._writableState.objectMode = true;
